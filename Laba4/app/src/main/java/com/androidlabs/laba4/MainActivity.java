5c4ffacb751387147f294b3236d88970ae7f7520
Pustovit Vladimir <vovapust1989@gmail.com>
2020-12-06 16:00:05 +0300

    Extract dims in res file.

--- Laba4/app/src/main/java/com/androidlabs/laba4/MainActivity.java
+++ Laba4/app/src/main/java/com/androidlabs/laba4/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "Main";
 
     private EditText etFirstOperand;
     private EditText etSecondOperand;
