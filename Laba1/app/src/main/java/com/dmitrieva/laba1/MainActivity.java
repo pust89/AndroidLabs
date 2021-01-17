e672365bddc612fd704a62deb41f9fc9effb11c4
Pustovit Vladimir <vovapust1989@gmail.com>
2021-01-17 18:32:14 +0300

    rename package

---
+++ Laba1/app/src/main/java/com/dmitrieva/laba1/MainActivity.java
@@ -0,0 +1,45 @@
+package com.dmitrieva.laba1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button firstButton;
+    private Button secondButton;
+    private Button thirdButton;
+    private Button fourthButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        setOnClickListener();
+    }
+
+    private void initViews() {
+        /* Находим вьюшки (которым будим менять цвета) по id */
+        firstButton = findViewById(R.id.btnOne);
+        secondButton = findViewById(R.id.btnTwo);
+        thirdButton = findViewById(R.id.btnThree);
+        fourthButton = findViewById(R.id.btnFour);
+    }
+
+    private void setOnClickListener() {
+        firstButton.setOnClickListener(this);
+        secondButton.setOnClickListener(this);
+        thirdButton.setOnClickListener(this);
+        fourthButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        ((Button) view).setText(getString(R.string.title_stars));
+    }
+}
---
+++ Laba3/app/src/main/java/com/dmitrieva/laba3/MainActivity.java
@@ -0,0 +1,70 @@
+package com.dmitrieva.laba3;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout parentLayout;
+
+    private TextView firstTextView;
+    private TextView secondTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        /* Находим вьюшки (которым будим менять цвета) по id */
+        parentLayout = findViewById(R.id.parentLayout);
+        firstTextView = findViewById(R.id.textView1);
+        secondTextView = findViewById(R.id.textView2);
+    }
+
+    /* Обработчик нажатия на кнопку  btnRedGreen */
+    public void btnRedGreenClick(View view){
+        firstTextView.setBackgroundColor(getResources().getColor(R.color.red));
+        firstTextView.setTextColor(getResources().getColor(R.color.green));
+        secondTextView.setBackgroundColor(getResources().getColor(R.color.red));
+        secondTextView.setTextColor(getResources().getColor(R.color.green));
+    }
+
+    /* Обработчик нажатия на кнопку  btnYellowBlue */
+    public void btnYellowBlueClick(View view){
+        firstTextView.setBackgroundColor(getResources().getColor(R.color.yellow));
+        firstTextView.setTextColor(getResources().getColor(R.color.blue));
+        secondTextView.setBackgroundColor(getResources().getColor(R.color.yellow));
+        secondTextView.setTextColor(getResources().getColor(R.color.blue));
+    }
+
+    /* Обработчик нажатия на кнопку  btnBlackWhite */
+    public void btnBlackWhiteClick(View view){
+        firstTextView.setBackgroundColor(getResources().getColor(R.color.black));
+        firstTextView.setTextColor(getResources().getColor(R.color.white));
+        secondTextView.setBackgroundColor(getResources().getColor(R.color.black));
+        secondTextView.setTextColor(getResources().getColor(R.color.white));
+    }
+
+    /* Обработчик нажатия на кнопку  btnRed */
+    public void btnRedClick(View view){
+        parentLayout.setBackgroundColor(getResources().getColor(R.color.red));
+    }
+
+    /* Обработчик нажатия на кнопку  btnBlue */
+    public void btnBlueClick(View view){
+        parentLayout.setBackgroundColor(getResources().getColor(R.color.blue));
+    }
+
+    /* Обработчик нажатия на кнопку  btnBlack */
+    public void btnBlackClick(View view){
+        parentLayout.setBackgroundColor(getResources().getColor(R.color.black));
+    }
+}
